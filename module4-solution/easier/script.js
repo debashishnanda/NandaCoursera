4f49b84a44e06c92e6c5f642d6f86641de5ef004
Debashish Nanda <49027391+CodeNanda@users.noreply.github.com>
2020-06-23 21:59:54 +0530

    changes

--- module4-solution/easier/script.js
+++ module4-solution/easier/script.js
@@ -33,6 +33,9 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+
+
+
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
@@ -59,9 +62,9 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+    byeGreeter.speak(names[i]);
   } else {
-    helloSpeaker.speak(names[i]);
+    helloGreeter.speak(names[i]);
   }
 }
 
